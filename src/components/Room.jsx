a17951ff0127d6d8a8291369aac36686d5a08c48
sachin kumar <goputen215@gmail.com>
2025-02-08 12:01:22 +0530

    Update Room.jsx

--- src/components/Room.jsx
+++ src/components/Room.jsx
@@ -2,7 +2,7 @@ import { useEffect, useRef, useState } from "react";
 import { useSearchParams } from "react-router-dom";
 import { io } from "socket.io-client";
 
-const URL = "http://localhost:3000";
+const URL = "https://free-talk-woad.vercel.app/";
 
 export const Room = ({ name, localAudioTrack, localVideoTrack }) => {
     const [searchParams, setSearchParams] = useSearchParams();
